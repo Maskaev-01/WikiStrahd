8914a50b132bfbf9291948dd9474a830f973e3d7
v_maskaev <maskaev-01@mail.ru>
2024-11-14 18:32:42 +0400

    Quartz sync: Nov 14, 2024, 6:32 PM

--- quartz/i18n/locales/ru-RU.ts
+++ quartz/i18n/locales/ru-RU.ts
@@ -30,7 +30,7 @@ export default {
       darkMode: "Тёмный режим",
     },
     explorer: {
-      title: "Проводник",
+      title: "Содержание",
     },
     footer: {
       createdWith: "Создано с помощью",
